ef7f2e9eaf0c561c3d6455cdf19d288e3ec024d9
Shibani Basava <shibanib@microsoft.com>
2023-11-07 19:30:04 +0000

    fix: change to use error codes passed

--- packages/chat-component/src/types.d.ts
+++ packages/chat-component/src/types.d.ts
@@ -92,7 +92,7 @@ declare type BotResponseMessage = Message & {
 };
 
 declare interface BotResponseError {
-  error?: string;
-  statusCode?: number;
-  message?: string;
+  error: string;
+  code: string;
+  message: string;
 }
